d65bbfecb3b03d3ef0a5e4255d13dcb2bb76d37f
thomas.holth <thomas.holth@gmail.com>
2022-08-22 11:46:41 +0200

    Create walls python lib

---
+++ antler/walls.py
@@ -0,0 +1,24 @@
+from rpw import revit, DB
+
+
+def split_wall_at_elevation(wall, elevation):
+    """
+    Elevation must be in internal units (feet)
+    """
+
+    # Check if wall intersects elevation
+    
+
+
+
+    # Make a copy of the wall. (Keep instance data)
+
+    wall_copy = None
+
+    # Set top constraints for original wall
+
+
+    # Set bottom constraints for new wall copy
+
+
+    return wall, wall_copy
